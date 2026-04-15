0e7370fbbdd1e4962bb91b93c76d1591224a3306
shanto <shantokumar22@gmail.com>
2026-04-16 00:24:36 +0600

    feat: create Dashboard page with user authentication and logout functionality

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,46 @@
+import { useAuthStore } from "@/store/useAuthStore"
+import { useNavigate } from "react-router"
+import { toast } from "sonner"
+import axios from "axios"
+import {useMutation} from "@tanstack/react-query"
+
 
 const Dashboard = () => {
+
+  const {user, token, logout} = useAuthStore()
+  const navigate = useNavigate()
+
+  const logOutMutation = useMutation({
+    mutationFn: async () =>{
+      const response = await axios.get('http://localhost:4000/logout', {
+        headers:{
+          Authorization: token
+        }
+      })
+      return response.data
+    },
+    onSuccess: (data)=>{
+      logout()
+      toast.success(data.message || "লগ-আউট সফল!")
+      navigate('/login')
+    },
+    onError: (error)=>{
+      toast.error(error.message || "লগ-আউট ব্যর্থ!")
+      logout()
+      navigate('/login')
+    }
+  })
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h1>ড্যাশবোর্ড</h1>
+      <p>স্বাগতম, {user?.name}</p>
+      <button 
+      disabled={logOutMutation.isPending}
+      onClick={() => logOutMutation.mutate()}>
+        {logOutMutation.isPending ? 'লগ-আউট হচ্ছে...' : 'লগ-আউট'}
+      </button>
+    </div>
   )
 }
 
