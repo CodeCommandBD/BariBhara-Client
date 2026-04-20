0688fd005a39fdbdd999bff8cbb20f0c57ce623d
shanto <shantokumar22@gmail.com>
2026-04-20 13:45:51 +0600

    feat: add property listing query and cache invalidation to useProperty hook

--- src/Hook/useProperty.ts
+++ src/Hook/useProperty.ts
@@ -1,21 +1,40 @@
 import { useAuthStore } from "@/store/useAuthStore";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { toast } from "sonner";
+
 
 export const useProperty = () => {
   const queryClient = useQueryClient();
   const { token } = useAuthStore();
 
+  // --- ১. সব প্রপার্টি লড করার জন্য কুয়েরি ---
+
+  const {data: properties, isLoading: isPropertiesLoading} = useQuery({
+    queryKey: ['my-properties'], // এটি দিয়ে ডাটা জমা থাকে
+    queryFn: async () => {
+      // 
+      const response = await axios.get("http://localhost:4000/api/property/my-property", {
+        headers: {
+          Authorization: token?.startsWith("Bearer ") ? token : `Bearer ${token}`, // টোকেন চেক করা হচ্ছে
+        },
+
+      });
+      return response.data.properties; // সার্ভার থেকে পাওয়া প্রপার্টিজ রিটার্ন করছি
+    },
+    enabled: !!token, // টোকেন থাকলে তবেই ডাটা আসবে
+  })
+
+// Add Property
   const createPropertyMutation = useMutation({
     mutationFn: async (formData: FormData) => {
       const response = await axios.post(
-        "http://localhost:4000/api/property/create",
+        "http://localhost:4000/api/property/add-property",
         formData,
         {
           headers: {
             "Content-Type": "multipart/form-data",
-            Authorization: token, // সিকিউরিটির জন্য টোকেন জরুরি
+            Authorization: token?.startsWith("Bearer ") ? token : `Bearer ${token}`, 
 
           },
         },
@@ -26,12 +45,14 @@ export const useProperty = () => {
         toast.success(data.message || "প্রপার্টি সফলভাবে তৈরি করা হয়েছে!")
 
         // বাড়ি সেভ হওয়ার পর ড্যাশবোর্ডের ডাটা অটোমেটিক রিফ্রেশ করবে
-        queryClient.invalidateQueries({queryKey: ['my-properties']})
+        queryClient.invalidateQueries({queryKey: ['my-properties']})  // লিস্ট রিলোড করবে
+
+        queryClient.invalidateQueries({ queryKey: ["dashboard-stats"] }); // ড্যাশবোর্ড আপডেট করবে
     },
     onError: (error: any) =>{
         toast.error(error.response?.data?.message ||  "কিছু একটা ভুল হয়েছে!")
     }
   });
 
-  return {createPropertyMutation}
+  return {createPropertyMutation, properties, isPropertiesLoading}
 };
