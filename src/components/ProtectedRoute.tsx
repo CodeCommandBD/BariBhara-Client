340a62e8ae0f0df7a89ba4a7d98d17f4c2bca22a
shanto <shantokumar22@gmail.com>
2026-05-16 14:18:44 +0600

    feat(client): optimize tenant portal layout and authentication routes

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -3,6 +3,10 @@ import { useAuthStore } from "@/store/useAuthStore"
 
 const ProtectedRoute = ({children}: {children: React.ReactNode}) => {
     const { isAuthenticated, user, logout } = useAuthStore((state) => state)
+    const hasHydrated = useAuthStore.persist.hasHydrated();
+
+    // লোড হওয়ার আগে রিডাইরেক্ট বন্ধ রাখা
+    if (!hasHydrated) return null;
     
     if(!isAuthenticated){
         return <Navigate to="/login" replace />
